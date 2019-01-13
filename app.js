d87781edcc16d80de4ac7b7570ae4772371f7d4d
Nicky Chen <nicky.chen0@gmail.com>
2019-01-13 18:14:40 -0500

    events

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ new Vue({
   data: {
     name: "Nick",
     job: "Developer",
-    website: "https://trivializer.netlify.com/"
+    website: "https://trivializer.netlify.com/",
+    websiteTag: "<a href='https://trivializer.netlify.com/'>New Website</a>",
+    age: 25,
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function(time) {
       return `Good ${time}, ${this.name}`;
+    },
+    add: function(number) {
+      this.age += number;
+    },
+    subtract: function(number) {
+      this.age -= number;
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
