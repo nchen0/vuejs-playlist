900ec8512997adb29605f7dee06a959b83252cfc
Nicky Chen <nicky.chen0@gmail.com>
2019-01-13 21:40:01 -0500

    dynamic css

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Nick",
+    name: "",
     job: "Developer",
     website: "https://trivializer.netlify.com/",
     websiteTag: "<a href='https://trivializer.netlify.com/'>New Website</a>",
-    age: 25,
+    age: 26,
     x: 0,
-    y: 0
+    y: 0,
+    a: 0,
+    b: 0,
+    available: false
   },
   methods: {
     greet: function(time) {
@@ -30,7 +33,19 @@ new Vue({
       this.name = e.target.value;
     },
     inputAge: function(e) {
-      console.log("age");
+      console.log("hello");
+      this.age = e.target.value;
+    },
+    changeColor: function() {
+      this.available = !this.available;
+    }
+  },
+  computed: {
+    addToA: function() {
+      return this.a + this.age;
+    },
+    addToB: function() {
+      return this.b + this.age;
     }
   }
 });
