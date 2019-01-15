8b02f4a93217d0210f942845dd57ed77445ed9b8
nchen0 <nicky.chen0@gmail.com>
2019-01-15 16:03:17 -0500

    props

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,15 +1,39 @@
+
+
 <template>
   <div>
+    <app-header></app-header>
+    <app-ninja v-bind:ninjas="ninjas"></app-ninja>
+    <hr>
     <h1>{{title}}</h1>
     <p>{{greeting()}}</p>
+    <ninjas></ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Ninjas from "./ninjas.vue";
+import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
+import Ninja from "./components/ninjas.vue";
 export default {
+  components: {
+    ninjas: Ninjas,
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninja": Ninja
+  },
   data() {
     return {
-      title: "First Vue File"
+      title: "First Vue File",
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML Wizardry", show: false },
+        { name: "Hitoshi", speciality: "Click Events", show: false },
+        { name: "Tango", speciality: "Conditionals", show: false },
+        { name: "Kami", speciality: "Webpack", show: false }
+      ]
     };
   },
   methods: {
@@ -20,5 +44,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+h1 {
+  color: purple;
+}
 </style>
---
+++ vuejs-playlist/src/ninjas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>List of Ninjas</h1>
+    <div>
+      <li v-for="ninja in ninjas">{{ ninja }}</li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ninjas: ["Yoshi", "Mario", "Bowser", "Ryu"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
